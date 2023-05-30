4762a2432838b0973b4f53a39c64d0f7c7265924
icaro <tony@axiomeconcept.com>
2023-05-30 10:35:37 +0200

    cmds

--- makeAct.go
+++ makeAct.go
@@ -10,6 +10,16 @@ import (
 
 func main() {
 	//Analizar si hay argumentos
+
+	alms := 0 //Numero de alarmas creadas
+	space := 10
+
+	cmds := 1 //Numero de cmds creados
+
+	comentario := "/**/" //Recupera los comentarios del fichero de configuracion cfg
+
+	lignes := 0 //Num de ligne
+
 	if len(os.Args) == 1 {
 		fmt.Println("Erreur args , example : makeAct nom_fichier ")
 		os.Exit(0)
@@ -33,9 +43,8 @@ func main() {
 
 	// Leer el contenido del archivo de entrada línea por línea
 	scanner := bufio.NewScanner(entradaFile)
-	comentario := "/**/" //Recupera los comentarios del fichero de configuracion cfg
-	alm_num := 0         //Numero de alarmas creadas
-	lignes := 0          //Num de ligne
+
+	//writeNumAlms(salidaFile, alms) //Write Num Alms Virtuelle
 
 	for scanner.Scan() {
 		linea := scanner.Text()
@@ -61,10 +70,20 @@ func main() {
 			}
 
 			// Escribir las entradas proporcionales al valor de ALM_VALUE en el archivo de salida
-			escribirEntradas(salidaFile, comentario, valor)
-			alm_num++
+			writeAlm(salidaFile, comentario, valor)
+			alms++
 		}
 	}
+
+	//Write cmd
+	writeNumCmds(salidaFile, cmds) //Write Num Alms Virtuelle
+
+	for cmd := space; cmd <= cmds*space; cmd += space {
+		writeCmd(salidaFile, "", cmd)
+
+	}
+
+	writeEnd(salidaFile)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error al leer el archivo de entrada:", err)
@@ -75,8 +94,13 @@ func main() {
 
 }
 
-// Función para escribir las entradas proporcionales al valor de ALM_VALUE en el alnrchivo de salida
-func escribirEntradas(salidaFile *os.File, comentario string, valor int) {
+func writeNumAlms(salidaFile *os.File, valor int) {
+	fmt.Fprintln(salidaFile, "/* COMMANDES */\n")
+	fmt.Fprintln(salidaFile, "NBR_ALM:", valor)
+	fmt.Fprintln(salidaFile, "\n")
+}
+
+func writeAlm(salidaFile *os.File, comentario string, valor int) {
 	fmt.Fprintln(salidaFile, comentario)
 	fmt.Fprintln(salidaFile, "ALM_NUM:", valor)
 	fmt.Fprintln(salidaFile, "ALM_OUT:", "0xF")
@@ -84,10 +108,54 @@ func escribirEntradas(salidaFile *os.File, comentario string, valor int) {
 	fmt.Fprintln(salidaFile, "ALM_CHG:", "0")
 	fmt.Fprintln(salidaFile, "ALM_STATUS:", "0x11")
 	fmt.Fprintln(salidaFile, "ALM_SCNEW:\tbegin")
-	fmt.Fprintln(salidaFile, "\tinfo")
-	fmt.Fprintln(salidaFile, "end")
+	fmt.Fprintln(salidaFile, "\t\t\t\tinfo")
+	fmt.Fprintln(salidaFile, "\t\t\tend")
 	fmt.Fprintln(salidaFile, "ALM_SCOLD:\tbegin")
-	fmt.Fprintln(salidaFile, "\tinfo")
-	fmt.Fprintln(salidaFile, "end")
+	fmt.Fprintln(salidaFile, "\t\t\t\tinfo")
+	fmt.Fprintln(salidaFile, "\t\t\tend")
 	fmt.Fprintln(salidaFile, "")
+}
+
+func writeNumCmds(salidaFile *os.File, valor int) {
+	fmt.Fprintln(salidaFile, "/* COMMANDES */")
+	fmt.Fprintln(salidaFile, "NBR_CMD:", valor)
+}
+
+func writeCmd(salidaFile *os.File, comentario string, valor int) {
+	fmt.Fprintln(salidaFile, comentario)
+	fmt.Fprintln(salidaFile, "CMD_NUM:", valor)
+	fmt.Fprintln(salidaFile, "CMD_OUT:", "0xF")
+	fmt.Fprintln(salidaFile, "CMD_STATUS:", "1")
+	fmt.Fprintln(salidaFile, "CMD_SC:", "\tbegin")
+	fmt.Fprintln(salidaFile, "\t\t\t\tinfo")
+	fmt.Fprintln(salidaFile, "\t\t\tend")
+	fmt.Fprintln(salidaFile, "")
+}
+
+func writeEnd(salidaFile *os.File) {
+
+	fmt.Fprintln(salidaFile, "DATE_SC:", "\tbegin")
+	fmt.Fprintln(salidaFile, "\t\t\tend\n")
+
+	fmt.Fprintln(salidaFile, "COUNTER_SC:", "begin")
+	fmt.Fprintln(salidaFile, "\t\t\tend\n")
+
+	fmt.Fprintln(salidaFile, "LOGIC_SC:", "\tbegin")
+	fmt.Fprintln(salidaFile, "\t\t\tend\n")
+
+	fmt.Fprintln(salidaFile, "CNTADD_SC:", "\tbegin")
+	fmt.Fprintln(salidaFile, "\t\t\tend\n")
+
+	fmt.Fprintln(salidaFile, "CNTADDF_SC:", "begin")
+	fmt.Fprintln(salidaFile, "\t\t\tend\n")
+
+	fmt.Fprintln(salidaFile, "MASKCMD0:", "\t{")
+	fmt.Fprintln(salidaFile, "\t\t\t}\n")
+
+	fmt.Fprintln(salidaFile, "MASKCMD1:", "\t{")
+	fmt.Fprintln(salidaFile, "\t\t\t}\n")
+
+	fmt.Fprintln(salidaFile, "MASKCMD2:", "\t{")
+	fmt.Fprintln(salidaFile, "\t\t\t}\n")
+
 }
